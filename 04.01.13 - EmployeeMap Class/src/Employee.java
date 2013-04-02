ccd73dd7b11823534ed4e5d15e00f016825844c8
Zachary Latta <zchlatta@gmail.com>
2013-04-02 13:26:28 -0700

    Added Javadoc for methods of Employee.java

--- 04.01.13 - EmployeeMap Class/src/Employee.java
+++ 04.01.13 - EmployeeMap Class/src/Employee.java
@@ -3,17 +3,33 @@ public class Employee
     private String name;
     private int id;
 
+    /**
+     * Creates a new Employee
+     * 
+     * @param name Employee's name
+     * @param id Employee's id
+     */
     public Employee(String name, int id)
     {
         this.name = name;
         this.id = id;
     }
 
+    /**
+     * Returns the id of the employee.
+     * 
+     * @return Id of the employee.
+     */
     public int getId()
     {
         return id;
     }
 
+    /**
+     * Returns the name of the employee.
+     * 
+     * @return Name of the employee.
+     */
     public String getName()
     {
         return name;
